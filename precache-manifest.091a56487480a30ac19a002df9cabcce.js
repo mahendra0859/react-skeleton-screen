61a2a983704083dd432cfd0a132bea7a7a50d030
Mahendra AR <mahendra0859@gmail.com>
2020-10-19 23:31:37 +0530

    Updates

---
+++ precache-manifest.091a56487480a30ac19a002df9cabcce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea48ccc4593183835a6c56d302602676",
+    "url": "/react-skeleton-screen/index.html"
+  },
+  {
+    "revision": "b6f7d24a9a3ef278d6b5",
+    "url": "/react-skeleton-screen/static/css/main.1016b190.chunk.css"
+  },
+  {
+    "revision": "d4aa2cb83949f0862438",
+    "url": "/react-skeleton-screen/static/js/2.da87261a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-skeleton-screen/static/js/2.da87261a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6f7d24a9a3ef278d6b5",
+    "url": "/react-skeleton-screen/static/js/main.54dc766d.chunk.js"
+  },
+  {
+    "revision": "66778f1e2e6c07545aae",
+    "url": "/react-skeleton-screen/static/js/runtime-main.3a983990.js"
+  }
+]);
